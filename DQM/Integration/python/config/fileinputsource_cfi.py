9c9d0f68c73599b2f0b5c0f502a781d3c9e1aa04
Nick Haubrich <nicholasjhaubrich@gmail.com>
2016-05-18 13:00:19 +0200

    First pass at EMTF quality tests

--- DQM/Integration/python/config/fileinputsource_cfi.py
+++ DQM/Integration/python/config/fileinputsource_cfi.py
@@ -3,7 +3,9 @@ import FWCore.ParameterSet.Config as cms
 source = cms.Source(
     "PoolSource",
     fileNames = cms.untracked.vstring(
-        #"root://eoscms//store/express/Commissioning2016/ExpressPhysics/FEVT/Express-v1/000/269/612/00000/0CAC70DE-3C01-E611-84B8-02163E01346D.root", 
+"/store/express/Run2016B/ExpressPhysics/FEVT/Express-v2/000/273/494/00000/0088D9BD-731B-E611-B349-02163E011DB6.root"    
+
+    #"root://eoscms//store/express/Commissioning2016/ExpressPhysics/FEVT/Express-v1/000/269/612/00000/0CAC70DE-3C01-E611-84B8-02163E01346D.root", 
         #"root://eoscms//store/express/Commissioning2016/ExpressPhysics/FEVT/Express-v1/000/269/612/00000/0EC9F919-4001-E611-BB95-02163E01391D.root",
         #"root://eoscms//store/express/Commissioning2016/ExpressPhysics/FEVT/Express-v1/000/269/612/00000/1CF4C821-3F01-E611-A25A-02163E012A04.root",
         #"root://eoscms//store/express/Commissioning2016/ExpressPhysics/FEVT/Express-v1/000/269/612/00000/586CE231-3E01-E611-8D60-02163E0127E3.root",
@@ -16,7 +18,7 @@ source = cms.Source(
         #"/store/data/Commissioning2016/ZeroBias1/RAW/v1/000/269/060/00000/1E91A739-1AFF-E511-A3CE-02163E011F71.root",
         #"root://eoscms//store/express/Commissioning2016/ExpressCosmics/FEVT/Express-v1/000/268/733/00000/5669AE2E-CAFC-E511-9402-02163E01452B.root",
         #"root://eoscms//store/express/Commissioning2016/ExpressCosmics/FEVT/Express-v1/000/268/733/00000/A459B232-CAFC-E511-B119-02163E011856.root",
-        "file:/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/DQMTest/MinimumBias__RAW__v1__165633__1CC420EE-B686-E011-A788-0030487CD6E8.root",
+        #"file:/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/DQMTest/MinimumBias__RAW__v1__165633__1CC420EE-B686-E011-A788-0030487CD6E8.root",
     )
  
     # For .dat files, use the following block instead.   
@@ -27,7 +29,7 @@ source = cms.Source(
 )
 
 maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(-1)
+    input = cms.untracked.int32(1000)
 )
 
 # Parameters for runType
